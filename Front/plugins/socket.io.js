ac010014e58e0419c3111a561cd52f076cf80692
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2022-01-07 12:25:43 +0100

    Update socket

--- Front/plugins/socket.io.js
+++ Front/plugins/socket.io.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-const API_URL = process.env.API_URL || 'http://localhost:3001'
+const API_URL = process.env.API_URL || 'http://localhost:3000'
 const socket = io(API_URL)
 
 export default socket
